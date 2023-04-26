732391c56d6ca6c90588de6f41657e745337cc61
Erik <erik.toroni@gmail.com>
2023-04-26 19:27:02 +0200

    App creation

---
+++ CustomAnimeLIstBackend/src/main/java/com/example/CustomAnimeList/CustomAnimeLIstBackend/CustomAnimeLIstBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.CustomAnimeList.CustomAnimeLIstBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomAnimeLIstBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomAnimeLIstBackendApplication.class, args);
+	}
+
+}
---
+++ CustomAnimeLIstBackend/src/test/java/com/example/CustomAnimeList/CustomAnimeLIstBackend/CustomAnimeLIstBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.CustomAnimeList.CustomAnimeLIstBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomAnimeLIstBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
