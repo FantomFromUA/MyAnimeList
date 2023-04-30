5f881201df411e0c94cba4c4c0fb5d05f22b3a71
Erik <erik.toroni@gmail.com>
2023-04-30 18:49:46 +0200

    Footer

--- custom-anime-list-frontend/src/App.tsx
+++ custom-anime-list-frontend/src/App.tsx
@@ -2,12 +2,14 @@ import React from 'react';
 import { BrowserRouter } from "react-router-dom";
 import Router from './pages/touter';
 import Header from './components/header/Header';
+import Footer from './components/footer/Footer';
 
 function App() {
   return (
     <BrowserRouter>
       <Header/>
       <Router />
+      <Footer/>
     </BrowserRouter>
   );
 }
---
+++ custom-anime-list-frontend/src/components/footer/Footer.tsx
@@ -0,0 +1,47 @@
+import React from 'react';
+import {
+    MDBFooter,
+    MDBContainer,
+    MDBBtn
+} from 'mdb-react-ui-kit';
+import {
+    FaGithub,
+    FaLinkedin,
+    FaInstagram,
+    FaGoogle,
+    FaHome,
+    FaQuestion
+} from 'react-icons/fa';
+
+
+
+const Footer = () => {
+    return (
+        <footer className="bg-dark text-center text-white" style={{position: "fixed", width: "100vw", height: "150px", top: "calc(100vh - 150px)"}}>
+            <div className="container p-4 pb-0">
+                <section className="mb-4">
+                    <a className="btn btn-outline-light btn-floating m-1" href="https://github.com/FantomFromUA!" role="button">
+                        <FaGithub />
+                    </a>
+                    <a className="btn btn-outline-light btn-floating m-1" href="https://www.linkedin.com/in/erik-toronyi-7b3320245/" role="button">
+                        <FaLinkedin />
+                    </a>
+                    <a className="btn btn-outline-light btn-floating m-1" href="https://www.instagram.com/fantom7_777/" role="button">
+                        <FaInstagram />
+                    </a>
+                    <a className="btn btn-outline-light btn-floating m-1" href="/" role="button">
+                        <FaHome />
+                    </a>
+                    <a className="btn btn-outline-light btn-floating m-1" href="/about" role="button">
+                        <FaQuestion />
+                    </a>
+                </section>
+            </div>
+            <div className="text-center p-3" style={{backgroundColor: "grey"}}>
+                © 2023 My Custom ANIME-LIST
+            </div>
+        </footer>
+    );
+}
+
+export default Footer;
